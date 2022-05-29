52eb8c138ba87d704d2e935dfab29c58b176bfb4
Payden Dyer <63562333+paydendyer@users.noreply.github.com>
2022-05-29 01:08:17 -0500

    Created homepage with Grid and added pages onto grid tiles

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Grid from '../components/Grid';
+
+const Home = () => {
+  return (
+    <div>
+    <h1 class="h1-home">Hi Payden, Welcome Home</h1> 
+    <Grid />
+    </div>
+  )
+}
+
+export default Home;

5058d60dcad595ba436ef4731cb47c6c4cb438c9
Payden Dyer <63562333+paydendyer@users.noreply.github.com>
2022-05-29 01:10:05 -0500

    Added weather page to App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import ToDo from './pages/ToDo';
 import Notes from './pages/Notes';
 import Calendar from './pages/Calendar';
 import ToggledarkMode from './components/ToggledarkMode';
+import Home from './pages/Home';
 import { useState } from 'react';
+import Budget from './pages/Budget';
+import Weather from './pages/Weather';
 const App = () => {
   const [darkMode, setDarkMode] = useState(false);
   return ( 
@@ -15,9 +18,12 @@ const App = () => {
   <BrowserRouter>
     <Navbar />
     <Routes>
+    <Route exact path='/Home' element={<Home/>} />
       <Route exact path='/ToDo' element={<ToDo/>} />
       <Route exact path='/Notes' element={<Notes/>} />
       <Route exact path='/Calendar' element={<Calendar/>} />
+      <Route exact path='/Budget' element={<Budget/>} />
+      <Route exact path='/Weather' element={<Weather/>} />
     </Routes>
   </BrowserRouter>
   </div>

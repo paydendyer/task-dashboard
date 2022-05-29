fd93bc5216ebc12ecfd741bf8c2c11b5c8b7f41c
Payden Dyer <63562333+paydendyer@users.noreply.github.com>
2022-05-29 01:12:39 -0500

    Added pages to grid tiles for Home page

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,18 +1,22 @@
 import React from "react";
 import styled from "styled-components";
 import { Responsive, WidthProvider } from "react-grid-layout";
+import Notes from '../pages/Notes';
+import ToDo from "../pages/ToDo";
+import Weather from "../pages/Weather";
+import {Link} from "react-router-dom";
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
+
+
 const layout = [
-  { i: "schedule", x: 0, y: 0, w: 2, h: 1 },
-  { i: "tile-two", x: 1, y: 0, w: 1, h: 1 },
+  { i: "tile-two", x: 0, y: 0, w: 1, h: 1 },
   { i: "tile-three", x: 2, y: 0, w: 1, h: 1 },
-  { i: "tile-four", x: 3, y: 0, w: 1, h: 1 },
-  { i: "tile-five", x: 4, y: 0, w: 1, h: 1 }
+  { i: "tile-four", x: 4, y: 0, w: 1, h: 1 },
 ];
 
 const GridItemWrapper = styled.div`
-  background: #f5f5f5;
+  background: none;
 `;
 
 const GridItemContent = styled.div`
@@ -30,25 +34,18 @@ export const Grid = () => {
       <ResponsiveGridLayout
         layouts={{ lg: layout }}
         breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
-        cols={{ lg: 5, md: 4, sm: 3, xs: 2, xxs: 1 }}
-        rowHeight={300}
+        cols={{ lg: 2, md: 2, sm: 2, xs: 2, xxs: 1 }}
+        rowHeight={700}
         width={1000}
       >
-        <GridItemWrapper key="schedule">
-          <GridItemContent>Today's Schedule</GridItemContent>
-        </GridItemWrapper>
+        <Link to={"../pages/Notes"}>View Full Page</Link>
         <GridItemWrapper key="tile-two">
-          <GridItemContent>Tile Two</GridItemContent>
+          <GridItemContent><Notes /></GridItemContent>
         </GridItemWrapper>
         <GridItemWrapper key="tile-three">
-          <GridItemContent>Tile Three</GridItemContent>
+          <GridItemContent><ToDo/></GridItemContent>
         </GridItemWrapper>
-        <GridItemWrapper key="tile-four">
-          <GridItemContent>Tile Four</GridItemContent>
-        </GridItemWrapper>
-        <GridItemWrapper key="tile-five">
-          <GridItemContent>Tile Five</GridItemContent>
-        </GridItemWrapper>
+        <GridItemWrapper key="tile-four"><GridItemContent><Weather/></GridItemContent></GridItemWrapper>
       </ResponsiveGridLayout>
     </Root>
   );

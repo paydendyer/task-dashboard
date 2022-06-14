4c6f3c94395f8260078c75d4d2b540de404eb159
Payden Dyer <63562333+paydendyer@users.noreply.github.com>
2022-06-13 21:44:18 -0500

    added content to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 const plugin = require('tailwindcss/plugin')
 module.exports = {
-  content: ['./src/components/SideBar.js', './src/components/'],
+  content: ['./src/components/SideBar.js', './src/components/Sidebar.js', './src/components/Budget/AddExpenseForm.js', './src/components/Budget/BudgetApp.js', './src/components/Notes/AddNote.js'],
   theme: {
     extend: {},
   },

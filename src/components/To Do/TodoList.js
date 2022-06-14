45edaab81f836fa40a9c9e9e39ea9a48acb30949
Payden Dyer <63562333+paydendyer@users.noreply.github.com>
2022-06-13 21:22:02 -0500

    Fixed nesting errors on TodoList.js

--- src/components/To Do/TodoList.js
+++ src/components/To Do/TodoList.js
@@ -1,55 +1,32 @@
-//Returns a list of todos
+// Returns a list of todos
 import React from 'react';
-import { MdDeleteForever } from 'react-icons/md';
+import {MdDeleteForever} from 'react-icons/md';
 import FlipMove from 'react-flip-move';
-import { useState } from 'react';
 
 
 function TodoList(props){
   const items = props.items;
-  const [check, setCheck] = useState(false);
   const listItems = items.map(item =>
-  {
-    return <div className="list" key={item.key}>
-      <p>
-      <div className={check ? "list__item list__item-checked" : "list__item"}>
-      <input
-        type="checkbox"
-        className="list__check"
-        checked={item.check}
-        onChange={() => setCheck(!check)}
-      />
-        <input 
-        type="text" 
-        data-testid="delete-todo"
-        id={item.key} 
-        value={item.text} 
-        onChange ={
-          (e) =>{
-            props.setUpdate(e.target.value, item.key)
-          }
-        } />
+ {
+     return <div className="list" key={item.key}>
+   <p>
+       <input type="text" id={item.key} value={item.text} onChange={(e)=>{
+           props.setUpdate(e.target.value,item.key)}}/>
       <span>
-      <MdDeleteForever className="todo-delete"
-        onClick={ () => props.deleteItem(item.key)
-
-        }
-      ></MdDeleteForever>
+     
+        <MdDeleteForever className="todo-delete" onClick={() => {
+          props.deleteItem(item.key)
+      }} icon="trash" />
       </span>
-      </div>
-    </p>
-    </div>
-  })
-  return(
-    
-    <div>
-      <FlipMove duration={500} easing="ease-in-out">
-        {listItems}
-        </FlipMove>
-      </div>
-
-  )
-
+   </p>
+   
+  </div>})
+  return <div>
+      <FlipMove duration={300} easing="ease-in-out">
+      {listItems}
+      </FlipMove>
+  
+  </div>;
 }
 
 export default TodoList;
